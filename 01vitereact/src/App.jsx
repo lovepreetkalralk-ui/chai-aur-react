0d406bf4e9c0638e1775ee72ad839c653434fb95
lovepreetkalralk-ui <lovepreetkalralk@gmail.com>
2025-11-03 13:08:46 +0000

    project using vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <><h1> chai aur react | Lovepreet </h1>
+    <h2>learn to make a project with vite</h2>
+   </>
+  );
+}
+
+export default App
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+
 # chai-aur-react
